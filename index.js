ce9a244028b67949b151848772cd792e12b72144
GarenLiang <ideabreeze@gmail.com>
2017-05-21 18:14:19 -0700

    menu bar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -9,7 +9,18 @@ app.on('ready', () => {
     titleBarStyle: 'hidden',
     //width: 1000,
     //frame: false;
-
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' }
+    ]
+  }
+];
